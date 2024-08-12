9551173ffda8d7a700dc3f6882fd8fa76a98f633
Oka Takaaki <fko2347014@stu.o-hara.ac.jp>
2024-08-12 11:14:45 +0900

    一覧表示と詳細表示のmodelの作成をしました

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Cooking, Detail
 
-# Register your models here.
+admin.site.register(Cooking)
+admin.site.register(Detail)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Cooking(models.Model):
+    cooking_title= models.CharField(max_length=200)
+    cooking_images = models.ImageField(upload_to='')
+
+class Detail(models.Model):
+    #class Cookingを継承
+    cooking = models.ForeignKey(Cooking, on_delete=models.CASCADE)
+    #主菜/副菜/主食/菓子
+    genre = models.CharField(max_length=100)
+    #材料
+    material = models.CharField(max_length=300)
+    #作り方の文章
+    steps_text = models.CharField(max_length=300)
+    #作り方の写真
+    steps_images = models.ImageField(upload_to='')
+    
+    #作り方の動画
+    #steps_movies = models.ImageField(upload_to='')
+    
+    #料理説明
+    detail_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+

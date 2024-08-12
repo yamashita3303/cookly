011c6142a0b6d87a523808be711467b814c272fe
Oka Takaaki <fko2347014@stu.o-hara.ac.jp>
2024-08-12 14:52:23 +0900

    ブランチ名の変更、modelの修正、home.htmlでタイトル一覧表示

--- app/models.py
+++ app/models.py
@@ -3,11 +3,11 @@ from django.db import models
 #料理全般の情報のテーブル
 class Recipe(models.Model):
     #料理タイトル
-    cooking_title= models.CharField(max_length=200)
+    recipe_title = models.CharField(max_length=200)
     #料理写真
-    cooking_images = models.ImageField(upload_to='recipes/')
+    recipe_image = models.ImageField(upload_to='recipes/')
     #詳細を開かれた回数
-    votes = models.IntegerField(default=0)
+    vote = models.IntegerField(default=0)
     #主菜/副菜/主食/菓子
     genre = models.CharField(max_length=100)
     #料理説明
@@ -24,13 +24,13 @@ class Ingredient(models.Model):
 
 class Step(models.Model):
     #class Recipeを継承
-    recipe = models.ForeignKey(Recipe, related_name='steps', on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, related_name='step', on_delete=models.CASCADE)
     #ステップの番号
     step_number = models.IntegerField()
     #作り方の文章
-    steps_text = models.CharField(max_length=300)
-    #作り方の写真
-    steps_images = models.ImageField(upload_to='steps/', blank=True, null=True)
-    #作り方の動画
-    steps_video = models.FileField(upload_to='videos/', blank=True, null=True)
+    step_text = models.CharField(max_length=300)
+    #作り方の写真   blank=True, null=Trueは任意で登録できるように
+    step_image = models.ImageField(upload_to='steps/', blank=True, null=True)
+    #作り方の動画   blank=True, null=Trueは任意で登録できるように
+    step_video = models.FileField(upload_to='videos/', blank=True, null=True)
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Recipe, Ingredient, Step
 
 def index(request):
     template = loader.get_template("app/index.html")
     return HttpResponse(template.render({}, request))
+
+def recipe(request):
+    recipes = Recipe.objects.all()
+    #recipesのlistになっている。
+    context = {'recipe_all': recipes}
+    template = loader.get_template("app/home.html")
+    return HttpResponse(template.render(context, request))

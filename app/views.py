f1924bc6a5a603f9c3c40606becf644d1a57377d
miltank241 <611umekota@gmail.com>
2024-08-21 23:28:23 +0900

    コードの整理をしました

--- app/views.py
+++ app/views.py
@@ -1,24 +1,20 @@
-from django.shortcuts import redirect
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.template import loader
-from .models import Recipe, Ingredient, Step
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.views import View
+from .models import Recipe, Ingredient, Step
 from .forms import RecipeForm, IngredientForm, StepForm
 
-
 def index(request):
-    template = loader.get_template("app/index.html")
-    return HttpResponse(template.render({}, request))
+    # render()にはrequest, htmlファイル名, 渡したいデータ(辞書型)を入れる
+    return render(request, "app/index.html")
 
 def recipe(request):
-    #インスタンス化
+    # インスタンス化
     recipes = Recipe.objects.all()
-    #リストに似たオブジェクトになっている。
+    # リストに似たオブジェクトになっている。
     context = {'recipe_all': recipes}
-    template = loader.get_template("app/home.html")
-    return HttpResponse(template.render(context, request))
+    return render(request, "app/home.html", context)
 
 def detail(request, post_id):
     recipes = Recipe.objects.get(id=post_id)
@@ -27,8 +23,7 @@ def detail(request, post_id):
         "recipe_chose": recipes,
         "ingredient_text": ingredient_text,
     }
-    template = loader.get_template("app/recipe.html")
-    return HttpResponse(template.render(context, request))
+    return render(request, "app/recipe.html", context)
 
 def search_recipes(request):
     # Recipeモデルの全てのインスタンスを取得
@@ -41,63 +36,67 @@ def search_recipes(request):
             Q(recipe_title__icontains=keyword)
         )
         messages.success(request, '[{}]の検索結果'.format(keyword))
-        return render(request, 'app/searchrecipes.html', {'search_recipes': search_recipes})
     else:
         # 検索キーワードがない場合
         messages.error(request, '検索キーワードが指定されていません。')
-        return render(request, 'app/searchrecipes.html', {'search_recipes': recipes})
+        search_recipes = recipes
     
-def recipe_create(request):
-    if request.method == "POST":
+    return render(request, 'app/searchrecipes.html', {'search_recipes': search_recipes})
+
+# 料理の見出し記入 
+class RecipeCreateView(View):
+    def get(self, request):
+        # フォームの表示
+        form = RecipeForm()
+        return render(request, 'app/form.html', {'form': form})
+
+    def post(self, request):
+        # データの保存
         form = RecipeForm(request.POST, request.FILES)
         if form.is_valid():
-            # 次へボタンを押したら詳細入力ページへ
+            # 必須項目が埋まっていたら保存後、ホームへ戻る
             recipe = form.save()
             return redirect('app:detail_create', recipe_id=recipe.id)
-        else:
-            # 入力していない項目あれば進まない
-            template = loader.get_template("app/form.html")
-            return HttpResponse(template.render({"form": form}, request))
-    else:
-        form = RecipeForm()
-        template = loader.get_template("app/form.html")
-        return HttpResponse(template.render({"form": form}, request))
+        return render(request, 'app/form.html', {'form': form})
 
-def detail_create(request, recipe_id):
-    recipe = Recipe.objects.get(id=recipe_id)
+# レシピの詳細記入
+class DetailCreateView(View):
+    def get(self, request, recipe_id):
+        return self.render_form(request, recipe_id)
 
-    if request.method == "POST":
+    def post(self, request, recipe_id):
+        recipe = get_object_or_404(Recipe, id=recipe_id)
         ingredient_form = IngredientForm(request.POST)
         step_form = StepForm(request.POST, request.FILES)
-
+        
         if ingredient_form.is_valid() and step_form.is_valid():
-            # `Ingredient` の保存
-            ingredient = ingredient_form.save(commit=False)
-            ingredient.recipe = recipe  # レシピに関連付ける
-            ingredient.save()
+            self.save_forms(ingredient_form, step_form, recipe)
+            return redirect('app:recipe')
+        
+        return self.render_form(request, recipe_id, ingredient_form, step_form)
 
-            # `Step` の保存
-            step = step_form.save(commit=False)
-            step.recipe = recipe  # レシピに関連付ける
-            step.save()
+    def save_forms(self, ingredient_form, step_form, recipe):
+        # 材料、ステップの保存
+        ingredient = ingredient_form.save(commit=False)
+        ingredient.recipe = recipe
+        ingredient.save()
 
-            # 保存後、別のページにリダイレクト（例えば、詳細ページ）
-            return redirect('app:detail', post_id=recipe.id)
-        else:
-            # フォームが無効な場合、エラーメッセージと共にフォームを再表示
-            template = loader.get_template("app/form_detail.html")
-            return HttpResponse(template.render({
-                "ingredient_form": ingredient_form,
-                "step_form": step_form,
-                "recipe": recipe
-            }, request))
+        step = step_form.save(commit=False)
+        step.recipe = recipe
+        step.save()
 
-    else:
+    def render_form(self, request, recipe_id):
+        # フォームの表示
+        recipe = get_object_or_404(Recipe, id=recipe_id)
         ingredient_form = IngredientForm()
         step_form = StepForm()
-        template = loader.get_template("app/form_detail.html")
-        return HttpResponse(template.render({
-            "ingredient_form": ingredient_form,
-            "step_form": step_form,
-            "recipe": recipe
-        }, request))
+        context = {
+            'ingredient_form': ingredient_form,
+            'step_form': step_form,
+            'recipe': recipe,
+        }
+        return render(request, 'app/form_detail.html', context)
+
+# classをview関数に変換
+recipe_create = RecipeCreateView.as_view()
+detail_create = DetailCreateView.as_view()

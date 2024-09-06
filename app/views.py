8f04208acc179b8835a6eff1904797d7600c5120
miltank241 <611umekota@gmail.com>
2024-09-07 02:00:50 +0900

    9/7 home.htmlの見た目を整え、views.pyにコメントを追加しました

--- app/views.py
+++ app/views.py
@@ -5,13 +5,17 @@ from django.views import View
 from .models import Recipe, Ingredient, Step
 from .forms import RecipeForm, IngredientForm, StepForm
 
+# 今は使ってない
 def index(request):
     # render()にはrequest, htmlファイル名, 渡したいデータ(辞書型)を入れる
     return render(request, "app/index.html")
 
+# レシピ一覧ページ(home.html)
 def recipe(request):
+    # GETリクエストからジャンルを取得
     selected_genre = request.GET.get('genre')
 
+    # ジャンルでのフィルタリングと同時に閲覧数の多い順に並べる
     if selected_genre:
         # 選択されたジャンルでフィルターをかける
         recipes = Recipe.objects.filter(genre=selected_genre).order_by('-vote')
@@ -23,10 +27,14 @@ def recipe(request):
     context = {'recipe_all': recipes, 'selected_genre': selected_genre}
     return render(request, "app/home.html", context)
 
+# レシピの詳細ページ(recipe.html)
 def detail(request, post_id):
+    # 指定されたIDのレシピを取得
     recipes = Recipe.objects.get(id=post_id)
+    # レシピに関連する材料の取得
     ingredient_text = recipes.ingredient.all()
 
+    # 閲覧数を1増やして保存
     recipes.vote += 1
     recipes.save()
 
@@ -36,11 +44,13 @@ def detail(request, post_id):
     }
     return render(request, "app/recipe.html", context)
 
+# レシピ検索ページ(searchrecipes.html)
 def search_recipes(request):
     # Recipeモデルの全てのインスタンスを取得
     recipes = Recipe.objects.all()
 
     # 検索機能の処理
+    # 入力したキーワードが含まれる料理名でフィルタリング
     keyword = request.GET.get('keyword')
     if keyword:
         search_recipes = recipes.filter(
@@ -54,50 +64,54 @@ def search_recipes(request):
     
     return render(request, 'app/searchrecipes.html', {'search_recipes': search_recipes})
 
-# 料理の見出し記入 
+
+# 料理の基本情報(タイトル、画像など)を追加するフォーム(form.html) 
 class RecipeCreateView(View):
+    # GETリクエストの処理
     def get(self, request):
         # フォームの表示
         form = RecipeForm()
         return render(request, 'app/form.html', {'form': form})
 
+    # POSTリクエストの処理
     def post(self, request):
-        # データの保存
+        # 送信されたデータをいったんformに入れる
         form = RecipeForm(request.POST, request.FILES)
         if form.is_valid():
             # 必須項目が埋まっていたら保存後、詳細入力ページへ
             recipe = form.save()
             return redirect('app:detail_create', recipe_id=recipe.id)
+        
+        # 必須項目が埋まってなければ再度フォームを表示
         return render(request, 'app/form.html', {'form': form})
 
-# レシピの詳細記入
+
+# レシピの詳細を記入するフォーム(form_detail.html)
 class DetailCreateView(View):
+    # GETリクエストの処理
     def get(self, request, recipe_id):
         return self.render_form(request, recipe_id)
 
+    # POSTリクエストの処理
     def post(self, request, recipe_id):
+        # 対象のレシピが存在しなければエラーを返す
         recipe = get_object_or_404(Recipe, id=recipe_id)
+        # フォームの表示
         ingredient_form = IngredientForm(request.POST)
         step_form = StepForm(request.POST, request.FILES)
         
+        # それぞれのフォームが有効ならば保存後、レシピ一覧ページへ
         if ingredient_form.is_valid() and step_form.is_valid():
             self.save_forms(ingredient_form, step_form, recipe)
             return redirect('app:recipe')
         
+        # フォームが無効なら再度フォームを表示
         return self.render_form(request, recipe_id, ingredient_form, step_form)
 
-    def save_forms(self, ingredient_form, step_form, recipe):
-        # 材料、ステップの保存
-        ingredient = ingredient_form.save(commit=False)
-        ingredient.recipe = recipe
-        ingredient.save()
 
-        step = step_form.save(commit=False)
-        step.recipe = recipe
-        step.save()
-
+    # get()から呼び出されるメソッド
+    # フォームを表示する
     def render_form(self, request, recipe_id):
-        # フォームの表示
         recipe = get_object_or_404(Recipe, id=recipe_id)
         ingredient_form = IngredientForm()
         step_form = StepForm()
@@ -107,6 +121,18 @@ class DetailCreateView(View):
             'recipe': recipe,
         }
         return render(request, 'app/form_detail.html', context)
+
+    # post()から呼び出されるメソッド
+    # 材料と手順を保存する
+    def save_forms(self, ingredient_form, step_form, recipe):
+        # 材料、ステップの保存
+        ingredient = ingredient_form.save(commit=False)
+        ingredient.recipe = recipe
+        ingredient.save()
+
+        step = step_form.save(commit=False)
+        step.recipe = recipe
+        step.save()
 
 # classをview関数に変換
 recipe_create = RecipeCreateView.as_view()

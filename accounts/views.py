599d934547820a65a634c7bbc8b19e794d88e558
yamashita <fko2347040@stu.o-hara.ac.jp>
2024-09-07 13:29:36 +0900

    signup、login、logout機能の実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from .models import CustomUser
 
@@ -8,28 +8,34 @@ def index(request):
     return render(request, 'index.html', context)
 
 def signup(request):
-    username = request.POST['username']
-    email = request.POST['email']
-    password = request.POST['password']
-    new_user = CustomUser(username=username, email=email, password=password)
-    new_user.save()
-    return HttpResponse('ユーザーの作成に成功しました')
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        new_user = CustomUser(username=username, email=email, password=password)
+        new_user.save()
+        return HttpResponse('ユーザーの作成に成功しました')
+    else:
+        return render(request, 'signup.html')
 
 def signin(request):
-    username = request.POST['username']
-    password = request.POST['password']
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
 
-    try:
-        user = CustomUser.objects.get(username=username)
-    except CustomUser.DoesNotExist:
-        return HttpResponse('ログインに失敗しました')
+        try:
+            user = CustomUser.objects.get(email=email)
+        except CustomUser.DoesNotExist:
+            return render(request, 'signin.html', {'error_message': 'ユーザーが存在しません。'})
 
-    if user.password == password:
-        login(request, user)  # これを実行するとユーザをログイン状態にできる
-        return HttpResponseRedirect('/accounts')
+        if user.password == password:
+            login(request, user)
+            return HttpResponseRedirect('/accounts')
+        else:
+            return render(request, 'signin.html', {'error_message': 'パスワードが正しくありません。'})
     else:
-        return HttpResponse('ログインに失敗しました')
-    
+        return render(request, 'signin.html')
+
 def signout(request):
     logout(request)
     return HttpResponseRedirect('/accounts')

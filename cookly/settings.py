c277d9683d5a7258b1061ee873db833bd7a5bf07
Takaaki Oka <fko2347014@stu.o-hara.ac.jp>
2024-10-09 14:12:10 +0900

    settingのDATABASESいじったお

--- cookly/settings.py
+++ cookly/settings.py
@@ -79,6 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
